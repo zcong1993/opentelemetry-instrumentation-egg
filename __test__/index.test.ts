3cb1a46291a7c4bc19ec5157f73fca24474528bb
zcong1993 <zhangcong1992@gmail.com>
2021-07-19 11:46:01 +0800

    feat: add test

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,3 +1,37 @@
+import { NodeTracerProvider } from '@opentelemetry/node'
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
+import { Resource } from '@opentelemetry/resources'
+import { ResourceAttributes } from '@opentelemetry/semantic-conventions'
+
+import {
+  SimpleSpanProcessor,
+  ConsoleSpanExporter,
+} from '@opentelemetry/tracing'
+
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { EggInstrumentation } from '../src'
+
+const setup = () => {
+  const serviceName = process.env.serviceName || 'server'
+
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL)
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [ResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  })
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
+
+  registerInstrumentations({
+    instrumentations: [new EggInstrumentation(), new HttpInstrumentation()],
+  })
+
+  provider.register()
+}
+
 it('should work well', () => {
-  expect(true).toBe(true)
+  setup()
 })
