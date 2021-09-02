3e466aa2119411af94e96244bb40b8691f43368a
zcong1993 <zhangcong1992@gmail.com>
2021-09-02 11:25:35 +0800

    fix: require version from package.json

--- src/version.ts
+++ src/version.ts
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { version } from '../package.json'
-export const VERSION = version
+export const VERSION: string = require('../package.json').version // eslint-disable-line @typescript-eslint/no-var-requires
